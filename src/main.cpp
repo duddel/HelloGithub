a5afd0e904c99980c2c98e33c4e051be2edb3d3f
duddel <duddel@users.noreply.github.com>
2019-02-27 20:28:28 +0100

    added a simple test program, cmake and travis files

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// enum classes are a c++11 feature!
+enum class Color{
+    yellow,
+    orange,
+    pink
+};
+
+int main()
+{
+    std::cout << "Hello Github!" << std::endl;
+    return 0;
+}
